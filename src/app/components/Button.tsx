a8ebc504d13ff1dc1dee22e30da2087397089cb8
Guilherme Poletti <gui.poletti@gmail.com>
2023-09-27 17:49:59 -0300

    Variants & Slots API

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,5 @@
 import { ComponentProps } from "react"
-import { tv } from 'tailwind-variants'
+import { tv, VariantProps } from 'tailwind-variants'
 
 const button = tv({
     base: [
@@ -7,14 +7,26 @@ const button = tv({
         'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500',
         'active:opacity-80',
     ],
+
+    variants:{
+        variant: {
+            primary: 'bg-violet-600 text-white hover:bg-violet-700',
+            ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none text-zinc-500',
+            outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
+        }
+    },
+
+    defaultVariants: {
+        variant: 'primary',
+    },
 })
 
-export type ButtonProps = ComponentProps<'button'>
-export function Button(props: ButtonProps) {
+export type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+export function Button({variant, className, ...props}: ButtonProps) {
     return (
         <button
             {...props}
-            className={button()}
+            className={button({variant, className})}
         />
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import * as FileInput from '@/app/components/Form/FileInput'
 import { Select } from "./components/Form/Select"
 import { SelectItem } from "./components/Form/Select/SelectItem"
 import { Textarea } from "./components/Form/Textarea"
+import { Button } from "./components/Button"
 export default function Home() {
   return (
     <>
@@ -21,19 +22,19 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
+            <Button
+              variant="outline"
               type="button"
-              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
             >
               Cancel
-            </button>
-            <button
+            </Button>
+            <Button
+              variant="primary"
               type="submit"
               form="settings"
-              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
             >
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -168,36 +169,36 @@ export default function Home() {
                 </Select>
 
                 <div className="flex items-center gap-1">
-                  <button
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3}/>
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3}/>
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <Link className="h-4 w-4 text-zinc-500" strokeWidth={3}/>
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <List className="h-4 w-4 text-zinc-500" strokeWidth={3}/>
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <ListOrdered className="h-4 w-4 text-zinc-500" strokeWidth={3}/>
-                  </button>                  
+                  </Button>                  
                 </div>
               </div>
 
@@ -227,18 +228,19 @@ export default function Home() {
 
           <div className="flex items-center justify-end gap-2 pt-5">
 
-            <button
+          <Button
+              variant="outline"
               type="button"
-              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
             >
               Cancel
-            </button>
-            <button
+            </Button>
+            <Button
+              variant="primary"
               type="submit"
-              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+              form="settings"
             >
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
